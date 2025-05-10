91d283f908487812924ada936f02be0e9771c6ab
Xendarboh <1435589+xendarboh@users.noreply.github.com>
2025-05-10 14:37:32 -0700

    build(ext): add tsconfig paths to vite plugins

--- apps/ext/wxt.config.ts
+++ apps/ext/wxt.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from 'wxt';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ['@wxt-dev/module-react'],
+
+  vite: () => ({
+    plugins: [tsconfigPaths()],
+  }),
 });
