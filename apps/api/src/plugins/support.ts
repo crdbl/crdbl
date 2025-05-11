18d745f1185913013380adb13cbbff5e191f76dc
Xendarboh <1435589+xendarboh@users.noreply.github.com>
2025-05-11 00:02:57 -0700

    style(api): fix lint error

--- apps/api/src/plugins/support.ts
+++ apps/api/src/plugins/support.ts
@@ -2,11 +2,17 @@ import fp from 'fastify-plugin'
 
 export interface SupportPluginOptions {
   // Specify Support plugin options here
+  enabled?: boolean
 }
 
 // The use of fastify-plugin is required to be able
 // to export the decorators to the outer scope
 export default fp<SupportPluginOptions>(async (fastify, opts) => {
+  if (opts.enabled === false) {
+    fastify.log.info('Support plugin is disabled, skipping initialization')
+    return
+  }
+
   fastify.decorate('someSupport', function () {
     return 'hugs'
   })
