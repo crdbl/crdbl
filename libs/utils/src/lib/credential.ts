8a210c22fc51dfa74fbaf73f762c3274f8f738f2
Xendarboh <1435589+xendarboh@users.noreply.github.com>
2025-05-11 17:18:49 -0700

    feat(utils): enhance createHolderDid with keypair return

--- libs/utils/src/lib/credential.ts
+++ libs/utils/src/lib/credential.ts
@@ -1,14 +1,26 @@
 import * as ed from '@noble/ed25519';
 import { base58btc } from 'multiformats/bases/base58';
 
-export async function createHolderDid() {
+export type HolderDidResult = {
+  did: string;
+  privateKey: string;
+  publicKey: string;
+};
+
+export async function createHolderDid(): Promise<HolderDidResult> {
+  // Create Ed25519 keypair entirely in the browser
   const priv = ed.utils.randomPrivateKey();
   const pub = await ed.getPublicKeyAsync(priv);
 
+  // Build a did:key – multicodec prefix 0xED01, then base58btc
   const prefixed = new Uint8Array(2 + pub.length);
   prefixed.set([0xed, 0x01]); // multicodec: Ed25519‑pub
   prefixed.set(pub, 2);
   const didKey = `did:key:z${base58btc.encode(prefixed)}`;
 
-  return didKey;
+  return {
+    did: didKey,
+    privateKey: ed.etc.bytesToHex(priv),
+    publicKey: ed.etc.bytesToHex(pub),
+  };
 }
