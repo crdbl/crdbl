c348b673f9d667a6f5682d5b78c6abeaff81bf22
Xendarboh <1435589+xendarboh@users.noreply.github.com>
2025-05-11 22:00:13 -0700

    chore: $ prettier --write ./apps/api ./apps/ext

--- apps/api/src/plugins/support.ts
+++ apps/api/src/plugins/support.ts
@@ -1,22 +1,22 @@
-import fp from 'fastify-plugin'
+import fp from 'fastify-plugin';
 
 export interface SupportPluginOptions {
   // Specify Support plugin options here
-  enabled?: boolean
+  enabled?: boolean;
 }
 
 // The use of fastify-plugin is required to be able
 // to export the decorators to the outer scope
 export default fp<SupportPluginOptions>(async (fastify, opts) => {
   if (opts.enabled === false) {
-    fastify.log.info('Support plugin is disabled, skipping initialization')
-    return
+    fastify.log.info('Support plugin is disabled, skipping initialization');
+    return;
   }
 
   fastify.decorate('someSupport', function () {
-    return 'hugs'
-  })
-})
+    return 'hugs';
+  });
+});
 
 // When using .decorate you have to specify added properties for Typescript
 declare module 'fastify' {
