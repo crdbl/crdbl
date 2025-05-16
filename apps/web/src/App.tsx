a2128374f43d6bf97d1d10c6597f239bfb26359d
Xendarboh <1435589+xendarboh@users.noreply.github.com>
2025-05-16 16:23:58 -0700

    feat(web): add embedded crdbls as examples

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -4,6 +4,13 @@ function App() {
   return (
     <div className="flex flex-col gap-4 prose">
       <h1>crdbl</h1>
+      <br />
+      <span>crdbl:MfJQRF9Jdr</span>
+      <span>crdbl:111111111</span>
+      <br />
+      This crdbl is inside of a `data-crdbl` tag'd div:
+      <div data-crdbl="MfJQRF9Jdr">me verifiable content here</div>
+      <div data-crdbl="XXXXXXXXXX">this not verifiable content here</div>
     </div>
   );
 }
