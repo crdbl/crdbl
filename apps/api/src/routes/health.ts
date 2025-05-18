4c0badfc069dc05c8ee6524cd0976bff1054dd84
Xendarboh <1435589+xendarboh@users.noreply.github.com>
2025-05-18 16:54:40 -0700

    refactor(api): reduce log noise in health check route

--- apps/api/src/routes/health.ts
+++ apps/api/src/routes/health.ts
@@ -3,18 +3,22 @@ import db from '../services/db.js';
 import ipfs from '../services/ipfs.js';
 
 const health: FastifyPluginAsync = async (fastify, _opts): Promise<void> => {
-  fastify.get('/health', async function (_request, reply) {
-    try {
-      await Promise.all([db.health(), ipfs.health()]);
-      return { status: 'ok' };
-    } catch (error) {
-      fastify.log.error(error);
-      return reply.status(503).send({
-        status: 'error',
-        message: 'health check failed',
-      });
+  fastify.get(
+    '/health',
+    { logLevel: 'warn' }, // reduce noisy logs
+    async function (_request, reply) {
+      try {
+        await Promise.all([db.health(), ipfs.health()]);
+        return { status: 'ok' };
+      } catch (error) {
+        fastify.log.error(error);
+        return reply.status(503).send({
+          status: 'error',
+          message: 'health check failed',
+        });
+      }
     }
-  });
+  );
 };
 
 export default health;
