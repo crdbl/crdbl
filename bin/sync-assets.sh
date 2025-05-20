d5b1328943545999af4cb0f328b24caab30c79da
Xendarboh <1435589+xendarboh@users.noreply.github.com>
2025-05-20 13:39:15 -0700

    feat(assets): add nx project with script to sync shared assets

---
+++ bin/sync-assets.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cp -v libs/assets/crdbl.svg          apps/ext/assets/
+cp -v libs/assets/crdbl_checked.svg  apps/ext/assets/
+cp -v libs/assets/crdbl_warning.svg  apps/ext/assets/
+
+cp -v libs/assets/crdbl.svg          apps/web/public/
