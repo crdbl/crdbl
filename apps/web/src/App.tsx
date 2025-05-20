e6815afb5d2dde10c7c0a7035776fa93a57a2ce0
Xendarboh <1435589+xendarboh@users.noreply.github.com>
2025-05-20 12:46:44 -0700

    style(web): update embedded crdbl demo layout

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -2,15 +2,21 @@ import './App.css';
 
 function App() {
   return (
-    <div className="flex flex-col gap-4 prose">
-      <h1>crdbl</h1>
-      <br />
-      <span>crdbl:MfJQRF9Jdr</span>
-      <span>crdbl:111111111</span>
-      <br />
-      This crdbl is inside of a `data-crdbl` tag'd div:
-      <div data-crdbl="MfJQRF9Jdr">me verifiable content here</div>
-      <div data-crdbl="XXXXXXXXXX">this not verifiable content here</div>
+    <div className="flex flex-col justify-center prose">
+      <h2>Crdbl: Building the Credible Web</h2>
+      <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4 w-full">
+        <legend className="fieldset-legend">embedded crdbl demo</legend>
+        <div className="flex flex-col gap-4 prose">
+          <label className="label">Text Reference:</label>
+          <span>crdbl:wTNhMzcM8g</span>
+          <span>crdbl:111111111</span>
+          <label className="label">HTML Attributes:</label>
+          <div data-crdbl="wTNhMzcM8g">verifiably credible content here</div>
+          <div data-crdbl="XXXXXXXXXX">
+            this content is not verifiably credible
+          </div>
+        </div>
+      </fieldset>
     </div>
   );
 }
